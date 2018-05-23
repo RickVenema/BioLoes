ebe57353364af1f1a511d186662c68a70357b58f
RickVenema <31951183+RickVenema@users.noreply.github.com>
2018-05-23 21:18:55 +0200

    created structure

---
+++ testfile.py
@@ -0,0 +1,44 @@
+###########
+# IMPORTS #
+###########
+# import BioLoes.BioLoesFASTA as BioLoes
+# from BioLoes import SortingAlgortihms
+# import BioLoes.BioLoesSEQ as SeqObj
+import BioLoes.BioLoesFUNCTIONS as Biofuncs
+import BioLoes.BioLoesBLAST as BLAST
+
+#############
+# Test Code #
+#############
+BLAST.BLASTn(81223361)
+
+#Biofuncs.translate("ATCTGACTATTCGATGCATGCAGCAGCAGACGCAGCATAGCGCATCGATCGA")
+#print(Biofuncs.find_ORFs("CATGAAAATGAAATAA"))
+
+# test = BioLoes.BioLoesFASTA("data/test.fasta")
+# print(test.fasta_data)
+# test.process_fasta()
+
+# print(test.object)
+# print(test.test())
+
+# for i in test.fasta_seq:
+#     print(i.sequence)
+#     print(i.length)
+# print(SortingAlgortihms.bubblesort([3, 4, 1, 2]))
+# print(SortingAlgortihms.selectionsort([3, 4, 1, 2]))
+
+#
+# seq_ojbect_Test = SeqObj.BioLoesSEQ("aaaCATGCATCAGCATGa", "coding")
+# print(seq_ojbect_Test.get_complement())
+#
+# print(seq_ojbect_Test.reverse_sequence())
+#
+# print(len(seq_ojbect_Test))
+# print(seq_ojbect_Test.upper())
+# print(seq_ojbect_Test.lower())
+
+
+
+
+
